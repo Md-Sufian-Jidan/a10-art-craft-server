9d13bd402538325c59fe94a638fe9cc8fa4b438b
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2024-04-26 14:50:09 +0600

    dynamic crafts created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
@@ -30,6 +30,12 @@ async function run() {
     app.get('/crafts', async(req, res) => {
       const cursor = craftsCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get('/crafts/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id : new ObjectId(id)}
+      const result = await craftsCollection.findOne(filter);
       res.send(result);
     });
 
